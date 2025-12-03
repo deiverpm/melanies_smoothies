493492ca466c5a81caef33d97b3bf7cf6e314e74
deiverpm <deiverpersonal@gmail.com>
2025-12-03 15:20:02 -0500

    Update streamlit_app.py
    Now with nutritional value

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen + ' ' # here the += means add this to what is already in the variable.
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -46,7 +48,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
       
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
